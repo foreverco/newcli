cf6cc0f71a9dbc558004fdffc6450249346da6c4
jay <huaxinlwm@163.com>
2019-12-03 11:06:43 +0800

    newcli

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img class="logo" src="./assets/logo.png" alt="logo">
+        <HelloWorld></HelloWorld>
+    </div>
+</template>
+
+<script type='text/ecmascript-6'>
+    import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        // 映射组件标签
+        components: {
+            HelloWorld
+        }
+    }
+</script>
+
+<style>
+    .logo{
+        width: 200px;
+        height :200px;
+        border:1px solid red;
+    }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='msg'>
+        {{msg}}
+    </div>
+</template>
+
+<script type='text/ecmascript-6'>   
+    export default { // 配置对象
+        data (){
+            return {
+                msg:'hello Vue Component'
+            }
+        }
+
+    }
+</script>
+
+<style>
+    .msg{
+        color:red;
+        font-size:30px;
+    }
+</style>
+ 
